07ffbb2d8a9d4d367c2b03f761dd3edfaa5fc1f8
regojoyson <rego.joyson@gmail.com>
2021-10-14 12:42:23 +0530

    renamed the folder

--- src/stories/components/button/Button.stories.tsx
+++ src/stories/components/button/Button.stories.tsx
@@ -5,7 +5,7 @@ import { Button } from './Button';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'B2B-Component/Button',
+  title: 'Customized-Components/Button',
   component: Button,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
